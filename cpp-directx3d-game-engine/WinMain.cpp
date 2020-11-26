d9736983cb9073d7f5e313adec910bce7c6714ae
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2020-11-26 17:18:31 +0100

    :sparkles: Create a window using the Windows API

--- cpp-directx3d-game-engine/WinMain.cpp
+++ cpp-directx3d-game-engine/WinMain.cpp
@@ -6,5 +6,44 @@ int CALLBACK WinMain(
 	LPSTR		lpCmdLine,
 	int			nCmdShow
 ) {
-	return 0;
+
+	// register window class
+	const wchar_t className[] = L"Main window";
+
+	WNDCLASSEX wc = { 0 };
+
+	wc.cbSize = sizeof( wc );
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = className;
+	wc.hIconSm = nullptr;
+
+	RegisterClassEx( &wc );
+
+	// create instance of window
+	HWND hWnd = CreateWindowEx(
+		0,
+		className,
+		L"My first Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200,
+		200,
+		640,
+		480,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr
+	);
+
+	if (hWnd == nullptr) return 0;
+	
+	ShowWindow(hWnd, nCmdShow);
 };
