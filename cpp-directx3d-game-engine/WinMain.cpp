8c3fa833fe6ee12ca12280e5cec56786ce9b04e7
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2020-11-30 17:11:34 +0100

    :Sparkes: Define custom Exception types

--- cpp-directx3d-game-engine/WinMain.cpp
+++ cpp-directx3d-game-engine/WinMain.cpp
@@ -1,88 +1,29 @@
-#include <Windows.h>
-#include <sstream>
-#include <string>
+#include "Window.h"
 
-LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-	switch (uMsg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(0);
-		break;
-	case WM_CHAR:
-	{
-		static std::wstring title;
-		title.push_back((char) wParam);
-
-		SetWindowText(hWnd, title.c_str());
-	}
-	break;
-	case WM_LBUTTONDOWN:
-	{
-		POINTS pt = MAKEPOINTS(lParam);
-		std::wostringstream wss;
-
-		wss << "(" << pt.x << ", " << pt.y << ")";
-
-		SetWindowText(hWnd, wss.str().c_str());
-	}
-	break;
-	}
-
-	return DefWindowProc(hWnd, uMsg, wParam, lParam);
-}
 
 int CALLBACK WinMain(
-	HINSTANCE	hInstance,
-	HINSTANCE	hPrevInstance,
-	LPSTR		lpCmdLine,
-	int			nCmdShow
-) {
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nCmdShow)
+{
+	Window wnd(800, 300, "Donkey Fart Box");
 
-	// register window class
-	const wchar_t className[] = L"Main window";
-
-	WNDCLASSEX wc = { 0 };
-
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WindowProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = className;
-	wc.hIconSm = nullptr;
-
-	RegisterClassEx(&wc);
-
-	// create instance of window
-	HWND hWnd = CreateWindowEx(
-		0,
-		className,
-		L"My first Window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200,
-		200,
-		640,
-		480,
-		nullptr,
-		nullptr,
-		hInstance,
-		nullptr
-	);
-
-	if (hWnd == nullptr) return 0;
-
-	ShowWindow(hWnd, nCmdShow);
-
-	// message loop
-	MSG msg = {};
-
-	while (GetMessage(&msg, nullptr, 0, 0)) {
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		// TranslateMessage will post auxilliary WM_CHAR messages from key msgs
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-};
+
+	// check if GetMessage call itself borked
+	if (gResult == -1)
+	{
+		return -1;
+	}
+
+	// wParam here is the value passed to PostQuitMessage
+	return msg.wParam;
+}
